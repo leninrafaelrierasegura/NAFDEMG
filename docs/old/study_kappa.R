fd00096fed100a925cd17d289d186048a1139696
Lenin Rafael Riera Segura <rierasl@stochproc-4.KAUST.EDU.SA>
2025-10-09 00:38:29 +0300

    almost done now tau convergence seems to be ok

--- docs/old/study_kappa.R
+++ docs/old/study_kappa.R
@@ -45,7 +45,9 @@ for (i in 1:length(KAPPAS)) {
 
 
 
+rmarkdown::render("control_conv_h_with_iteration.Rmd")
 rmarkdown::render("control_conv_m_with_iteration.Rmd")
+
 KAPPAS <- c(4,8,16,32)
 DIVIDER <- c(1,2,4,8,16)
 for (i in 1:length(KAPPAS)) {
@@ -54,7 +56,7 @@ for (i in 1:length(KAPPAS)) {
   for (j in 1:length(DIVIDER)) {
     divider <- DIVIDER[j]
     saveRDS(divider, "old/divider.RDS")
-    rmarkdown::render("control_conv_h_with_iteration.Rmd")
+    #rmarkdown::render("control_conv_h_with_iteration.Rmd")
     rmarkdown::render("control_conv_tau_with_iteration.Rmd")
   }
 }
